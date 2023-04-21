6c0e71a029b958f3ff1034ceda2c6a45b8619d64
AlexKozhanov <alexander_kozhanov@mail.ru>
2023-04-21 19:51:55 +0500

    hotkeys

--- HolloWorld.py
+++ HolloWorld.py
@@ -2,4 +2,16 @@ if __name__ == '__main__':
     pass
     print('Hello, world!')
 
+    # todoо: Players
 
+    # region important
+    print('1)сtrl + shift + F10 - запускает код')
+    print('2)колёсико мыши находит использование объекта,')
+    print('ctrl + лкм - момент объявления ')
+    print('shift + F6 - реформат переменных и не только')
+    print('alt + лкм - позволяет создавать любое кол-во синхронизированных курсоров')
+    print('ctrl + alt + s - открывает настройки')
+    print('ctrl + alt + t - реформат кода в файле')
+    print('ctrl + alt + о - оптимизация импортов в файле')
+    print('ctrl + alt + F12 - быстрый переход в проводник')
+# endregion
--- Labo2.py
+++ Labo2.py
@@ -1,7 +1,7 @@
 command = input("номер лабы:")
 match command.split():
-    case ["1"]: # 1.1 заменить буквы на цифры
+    case ["1"]:  # 2.1 Опишите себя, используя список
         str = "zxcvbnhy yhnbvcxz xxx yyy"
         str2 = "lala"
-    case ["2"]: # 1.2 сосчитать произведение чисел, кратных 3 и 5
+    case ["2"]:  # 1.2 Опишите себя, используя словарь
         str = "zxcvbnhy yhnbvcxz xxx yyy"
