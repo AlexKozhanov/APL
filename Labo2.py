190644585306b1a5a109557c775b5ce767508e9c
AlexKozhanov <alexander_kozhanov@mail.ru>
2023-04-21 17:08:20 +0500

    добавлена строка str2

--- Labo2.py
+++ Labo2.py
@@ -2,5 +2,6 @@ command = input("номер лабы:")
 match command.split():
     case ["1"]: # 1.1 заменить буквы на цифры
         str = "zxcvbnhy yhnbvcxz xxx yyy"
+        str2 = "lala"
     case ["2"]: # 1.2 сосчитать произведение чисел, кратных 3 и 5
         str = "zxcvbnhy yhnbvcxz xxx yyy"
