138c6e84e3c817f4e3307f85be3b831db59868d3
AlexKozhanov <alexander_kozhanov@mail.ru>
2023-05-06 20:04:34 +0500

    Лаба4

--- HolloWorld.py
+++ HolloWorld.py
@@ -26,3 +26,23 @@ if __name__ == '__main__':
     #     main()
 
     # Заслонение - наименование переменной совпадает с именами функции, типов, ...
+
+    # import x                # получить х
+    # import x as y           # получить х как у
+    # from x import y         # только у из х
+    # from x import y as z    # только у из х как z
+    # from x import *         # Так плохо! Но можно, но перед этим нужно извиниться
+    # пример
+    #
+    # x = numpy.array(range(100))
+    # print(numpy.mean(x))
+    # import numpy as np  # 2)- так короче
+    #
+    # x = np.array(range(100))
+    # print(np.mean(x))
+    # from numpy import array  # 3) - можно через запятую
+    #
+    # x = array(range(100))
+    # print(x.mean())
+    # print(np.mean())       - не работает, т.к. имя numpy не импортировано
+    # print(numpy.mean())    - не работает, т.к. имя numpy не импортировано
--- Labo4.py
+++ Labo4.py
@@ -1,25 +1,18 @@
+def task2(x):
+    print('Введите значение: ')
+    input(x)
+
+
 def main():
-    pass
+    import numpy as np
+    array1 = np.array(range(101))
+    # sum_array = array1.sum()
+    print(array1.sum())
 
-    # import x                # получить х
-    # import x as y           # получить х как у
-    # from x import y         # только у из х
-    # from x import y as z    # только у из х как z
-    # from x import *         # Так плохо! Но можно, но перед этим нужно извиниться
-    # пример
-    import numpy  # 1)
 
-    x = numpy.array(range(100))
-    print(numpy.mean(x))
-    import numpy as np  # 2)- так короче
-
-    x = np.array(range(100))
-    print(np.mean(x))
-    from numpy import array  # 3) - можно через запятую
-
-    x = array(range(100))
-    print(x.mean())
-    # print(np.mean())       - не работает, т.к. имя numpy не импортировано
-    # print(numpy.mean())    - не работает, т.к. имя numpy не импортировано
-
+if __name__ == "__main__":
     main()
+    # 1) Сумма ряда 0-100
+    # 2) Сумма ряда 0-input
+    # 3) Среднее среди 100 случайных чисел
+  
