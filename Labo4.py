d42252e89d388fc70213cd50ab6d606715bae629
AlexKozhanov <alexander_kozhanov@mail.ru>
2023-05-07 16:41:38 +0500

    Лаба4 готова

--- Labo4.py
+++ Labo4.py
@@ -1,18 +1,35 @@
-def task2(x):
-    print('Введите значение: ')
-    input(x)
-
-
-def main():
+def task1():
+    # 1) Сумма ряда 0-100
     import numpy as np
     array1 = np.array(range(101))
     # sum_array = array1.sum()
     print(array1.sum())
 
 
-if __name__ == "__main__":
-    main()
-    # 1) Сумма ряда 0-100
+def task2():
     # 2) Сумма ряда 0-input
+    import numpy as np
+    x = int(input('Введите значение: '))
+    array2 = np.array(range(0, x + 1))
+    print(array2.sum())
+
+
+def task3():
     # 3) Среднее среди 100 случайных чисел
-  
+    import numpy as np
+    np.random.seed(1000)
+    a = np.random.random(100)
+    # print(a)
+    # print(a.sum(), ' / ', a.size, ' = ', a.sum() / a.size)
+    print('Среднее значение массива из 100 случайных чисел = ', a.sum() / a.size)
+
+
+if __name__ == "__main__":
+    command = input("Номер задания:")
+    match command.split():
+        case ["1"]:
+            task1()
+        case ["2"]:
+            task2()
+        case ["3"]:
+            task3()
