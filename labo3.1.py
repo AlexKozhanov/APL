64ae7bf66299c135a574b8bd040b6ce3de4b08fc
AlexKozhanov <alexander_kozhanov@mail.ru>
2023-05-08 08:39:35 +0500

    08.05

--- labo3.1.py
+++ labo3.1.py
@@ -1,16 +1,36 @@
 # -*- coding: cp1251 -*-
 def main():
     # dictionary словарь
-    person = {
-        "courses": "Ресурсное",
-        "age": 36,
-        "name": "Андрейченко Игорь Леонардович",
-        "sex": "male",
-        "position": "Доцент",
-        "science": ""
+    persons = {
+        "Андрей":{
+            "age": 36,
+            "height": 180,
+            "sex": "male",
+            "position": "Доцент"
+        },
+        "Александр": {
+            "age": 36,
+            "height": 170,
+            "sex": "male",
+            "position": "Доцент"
+        },
+        "Ольга": {
+            "age": 36,
+            "height": 165,
+            "sex": "woman",
+            "position": "Ст.преподаватель"
+        }
     }
-    for k, v in person.items():
-        print(f"{k} >>> {v}")
+    for username, userinfo in persons.items():
+        print(f"Имя пользователя: {username}")
+        age = userinfo["age"]
+        sex = userinfo["sex"]
+
+        print(f"Возраст пользователя {username} : {age} лет.")
+        print(f"Пол пользователя {username} : {sex}.\n")
+
+    # for k, v in person.items():
+    #     print(f"{k} >>> {v}")
 
 
 if __name__ == '__main__':
