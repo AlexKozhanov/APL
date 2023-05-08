beb35df237f91415dbed25056f826bc928101f68
AlexKozhanov <alexander_kozhanov@mail.ru>
2023-05-08 12:02:54 +0500

    Лаб4 выполнены задания 1 и 2

--- Labo4.py
+++ Labo4.py
@@ -1,32 +1,31 @@
+# -*- coding: cp1251 -*-
 import numpy as np
-
-
 def task1():
-    # 1) РЎСѓРјРјР° СЂСЏРґР° 0-100
+    # 1) Сумма ряда 0-100
     array1 = np.array(range(101))
     # sum_array = array1.sum()
     print(array1.sum())
 
 
 def task2():
-    # 2) РЎСѓРјРјР° СЂСЏРґР° 0-input
-    x = int(input('Р’РІРµРґРёС‚Рµ Р·РЅР°С‡РµРЅРёРµ: '))
+    # 2) Сумма ряда 0-input
+    x = int(input('Введите значение: '))
     array2 = np.array(range(0, x + 1))
     # array2 = np.arange(0, x + 1)
     print(array2.sum())
 
 
 def task3():
-    # 3) РЎСЂРµРґРЅРµРµ СЃСЂРµРґРё 100 СЃР»СѓС‡Р°Р№РЅС‹С… С‡РёСЃРµР»
+    # 3) Среднее среди 100 случайных чисел
     np.random.seed(1000)
     a = np.random.random(100)
     # print(a)
     # print(a.sum(), ' / ', a.size, ' = ', a.sum() / a.size)
-    print('РЎСЂРµРґРЅРµРµ Р·РЅР°С‡РµРЅРёРµ РјР°СЃСЃРёРІР° РёР· 100 СЃР»СѓС‡Р°Р№РЅС‹С… С‡РёСЃРµР» = ', a.sum() / a.size)
+    print('Среднее значение массива из 100 случайных чисел = ', a.sum() / a.size)
 
 
 if __name__ == "__main__":
-    command = input("РќРѕРјРµСЂ Р·Р°РґР°РЅРёСЏ:")
+    command = input("Номер задания:")
     match command.split():
         case ["1"]:
             task1()
--- labo3.1.py
+++ labo3.1.py
@@ -1,37 +1,87 @@
 # -*- coding: cp1251 -*-
+def find_persons_in_dict(persons):
+    people_dict = {} # dictionary словарь
+    for username, userinfo in persons.items():
+        age = userinfo["age"]
+        height = userinfo["height"]
+        sex = userinfo["sex"]
+        position = userinfo["position"]
+
+        people_dict.update({username: {
+            'age': age,
+            'height': height,
+            'sex': sex,
+            'position': position,
+        }})
+    # print(persons)
+    # print(people_dict)
+    sorted_dict = { # сортировка словаря по возрастанию возраста
+        k: v for k, v in sorted(people_dict.items(), key=lambda x:x[1]['age'])
+    }
+    last = list(sorted_dict.keys())[-1]
+    # print(sorted_dict)
+    # print(last)
+    # print(people_dict[last])
+    return last, people_dict[last]
+
+def find_teacher_in_list(persons):
+    persons_list = [] # list список
+    for username, userinfo in persons.items():
+        age = userinfo["age"]
+        height = userinfo["height"]
+        sex = userinfo["sex"]
+        position = userinfo["position"]
+
+        persons_list += [( # конвертируем словарь в список с кортежами
+            username,
+            age,
+            height,
+            sex,
+            position,
+        )]
+    # print(persons_list)
+    rez = sorted(persons_list, key=lambda x: x[1])[-1] # сортировка списка по возрастанию возраста
+    # persons_list = sorted(persons_list, key=lambda x: x[1])
+    # print(persons_list)
+    return rez
+
 def main():
-    # dictionary словарь
     persons = {
-        "Андрей":{
+        "Andriy":{
             "age": 36,
             "height": 180,
             "sex": "male",
-            "position": "Доцент"
+            "position": "docent"
         },
-        "Александр": {
-            "age": 36,
+        "Alexsander": {
+            "age": 40,
             "height": 170,
             "sex": "male",
-            "position": "Доцент"
+            "position": "docent"
         },
-        "Ольга": {
-            "age": 36,
+        "Olga": {
+            "age": 30,
             "height": 165,
             "sex": "woman",
-            "position": "Ст.преподаватель"
+            "position": "senior lecturer"
         }
     }
-    for username, userinfo in persons.items():
-        print(f"Имя пользователя: {username}")
-        age = userinfo["age"]
-        sex = userinfo["sex"]
-
-        print(f"Возраст пользователя {username} : {age} лет.")
-        print(f"Пол пользователя {username} : {sex}.\n")
-
-    # for k, v in person.items():
-    #     print(f"{k} >>> {v}")
+    the_oldest_person = find_persons_in_dict(persons)
+    print('Max old from dictionary - ', the_oldest_person)
+    the_oldest_person = find_teacher_in_list(persons)
+    print("Max old from list - ", the_oldest_person)
 
 
 if __name__ == '__main__':
     main()
+
+    # for username, userinfo in persons.items():
+    #     print(f"Имя пользователя: {username}")
+    #     age = userinfo["age"]
+    #     sex = userinfo["sex"]
+    #
+    #     print(f"Возраст пользователя {username} : {age} лет.")
+    #     print(f"Пол пользователя {username} : {sex}.\n")
+
+    # for k, v in person.items():
+    #     print(f"{k} >>> {v}")
