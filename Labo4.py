a9a7774602c140b1bcf8d24bd3699139321dbe79
Admin <Admin@mail.com>
2023-05-24 23:01:05 +0500

    кодировка UTF-8

--- Labo4.py
+++ Labo4.py
@@ -5,14 +5,12 @@ def task1():
     # sum_array = array1.sum()
     print(array1.sum())
 
-
 def task2():
     # 2) Сумма ряда 0-input
     x = int(input('Введите значение: '))
     array2 = np.array(range(0, x + 1))
     # array2 = np.arange(0, x + 1)
     print(array2.sum())
-
 
 def task3():
     # 3) Среднее среди 100 случайных чисел
@@ -21,7 +19,6 @@ def task3():
     # print(a)
     # print(a.sum(), ' / ', a.size, ' = ', a.sum() / a.size)
     print('Среднее значение массива из 100 случайных чисел = ', a.sum() / a.size)
-
 
 if __name__ == "__main__":
     command = input("Номер задания:")
